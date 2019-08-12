002db8cda5c46b36a9996dff3c1f9cae2a951c90
duxinyuan.ex <duxinyuan.ex@hx-partners.com>
2019-08-12 09:28:35 +0800

    zhi分支改动

--- app/src/main/java/com/example/myapplication32/MainActivity.java
+++ app/src/main/java/com/example/myapplication32/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication32;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         //niguola
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    int size = 30;
+    //zhi分支改动
+    public void bigger(View view) {
+        Button button = (Button)findViewById(R.id.joe);
+        button.setTextSize(++size);
     }
 }
